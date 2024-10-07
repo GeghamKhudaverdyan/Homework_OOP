1788a6fc16aa68f1aa263f7b70a23bc4118f7f8f
GeghamKhudaverdyan <geghamkhudaverdyan686@gmail.com>
2024-10-07 19:04:00 +0400

    Add files via upload
    Signed-off-by: GeghamKhudaverdyan <geghamkhudaverdyan686@gmail.com>

---
+++ virtualPetSimulator/hpp/petAdoptionCenter.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "type.hpp"
+#include "AbstractPetFactory.hpp"
+#include "domesticPetFactory.hpp"
+#include "wildPetFactory.hpp"
+
+class AbstractPetFactory;
+
+class PetAdoptionCenter {
+public:
+    static PetAdoptionCenter* getInstance(Type type);
+    
+private:
+    PetAdoptionCenter(Type type);
+    AbstractPetFactory* m_abstractP; 
+    static PetAdoptionCenter* m_ptr;
+};
